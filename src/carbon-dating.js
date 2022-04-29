da26a7287b6fba32394d5479053baeb0dc865125
sahion <alexeysuvorov33@gmail.com>
2022-04-29 13:51:27 +0700

    make work carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if(!sampleActivity || typeof sampleActivity != 'string' || isNaN(+sampleActivity) || +sampleActivity<=0 || +sampleActivity>15) return false;
+  sampleActivity = +sampleActivity;
+  let k = 0.693/5730;
+  let t = Math.ceil(Math.log(15/sampleActivity)/k);
+
+  return t;
 }
 
 module.exports = {
