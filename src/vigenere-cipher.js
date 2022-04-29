2f0b9a34567def4e522866302c524519b74e6c16
sahion <alexeysuvorov33@gmail.com>
2022-04-30 01:13:52 +0700

    make vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,57 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct){
+    this.direct = (direct == false)? false : true;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   encoder = new TextEncoder();
+   decoder = new TextDecoder();
+   result = '';
+  encrypt(message,code) {
+    if (!message || !code) {throw Error('Incorrect arguments!')}
+    let messageUTF = this.encoder.encode(message.toUpperCase());
+    let codeUTF = this.encoder.encode(code.toUpperCase());
+    let codeCounter = 0;
+    for (let i in messageUTF){
+      if(messageUTF[i] > 64 && messageUTF[i]<91) {
+        messageUTF[i]+=codeUTF[codeCounter]-65;
+        if (messageUTF[i]>90) {messageUTF[i]-=26};
+        codeCounter++;
+        if (codeCounter==codeUTF.length){codeCounter=0;}
+      }
+    }
+    let messageDecoded = this.decoder.decode(messageUTF);
+    if (!this.direct){
+      for (let i=messageDecoded.length-1; i>=0; i-- ){
+        this.result+=messageDecoded[i];
+      }
+      messageDecoded=this.result;
+    }
+    this.result='';
+    return messageDecoded;
+  }
+  decrypt(message,code) {
+    if (!message || !code) {throw Error('Incorrect arguments!')}
+    let messageUTF = this.encoder.encode(message.toUpperCase());
+    let codeUTF = this.encoder.encode(code.toUpperCase());
+    let codeCounter = 0;
+    for (let i in messageUTF){
+      if(messageUTF[i] > 64 && messageUTF[i]<91) {
+        messageUTF[i]-=codeUTF[codeCounter]-65;
+        if (messageUTF[i]<65) {messageUTF[i]+=26};
+        ++codeCounter;
+        if (codeCounter==codeUTF.length){codeCounter=0;}
+      }
+    }
+    let messageDecoded = this.decoder.decode(messageUTF);
+    if (!this.direct){
+      for (let i=messageDecoded.length-1; i>=0; i-- ){
+        this.result+=messageDecoded[i];
+      }
+      messageDecoded=this.result;
+    }
+    this.result='';
+    return messageDecoded
   }
 }
 
